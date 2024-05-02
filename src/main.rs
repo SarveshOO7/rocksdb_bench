9607be3176ad9c6531a5e851c1578ab0596966a2
SarveshOO7 <sarveshtandonus@gmail.com>
2024-05-02 10:10:16 -0400

    more testing

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
 
 use rand::distributions::Bernoulli;
 use rand::{distributions::Distribution, Rng};
-use rocksdb::{DBPath, DBWithThreadMode, MultiThreaded, DB};
+use rocksdb::{BlockBasedOptions, Cache, DBPath, DBWithThreadMode, MultiThreaded, Options, DB};
 use std::sync::Arc;
 use std::{
     ops::{Deref, DerefMut},
@@ -16,10 +16,22 @@ use tracing::trace;
 use tokio::task::LocalSet;
 
 static COUNTER: AtomicUsize = AtomicUsize::new(0);
+static READ_COUNTER: AtomicUsize = AtomicUsize::new(0);
 
 const PAGE_SIZE: usize = 4 * 1024;
 const GIGABYTE: usize = 1024 * 1024 * 1024;
 const GIGABYTE_PAGES: usize = GIGABYTE / PAGE_SIZE;
+
+#[test]
+fn temp() {
+    let mut rng = rand::thread_rng();
+    for i in 0..1000 {
+        // let mut zipf = zipf::ZipfDistribution::new(100, 0.1).unwrap();
+        // let id = zipf.sample(&mut rng) as u64;
+        let id = rng.gen_range(0..100);
+        println!("{}", id);
+    }
+}
 
 #[test]
 fn bench() {
@@ -33,24 +45,22 @@ fn bench() {
     const FRAMES: usize = GIGABYTE_PAGES;
     const DISK_PAGES: usize = 32 * GIGABYTE_PAGES;
 
-    let coin = Bernoulli::new(0.2).unwrap();
+    let coin = Bernoulli::new(0.5).unwrap();
 
     println!("Operations: {OPERATIONS}");
+
+    let mut opts = BlockBasedOptions::default();
+    let cache = Cache::new_lru_cache(GIGABYTE);
+    opts.set_block_cache(&cache);
+
+    let mut cf_opts = Options::default();
+    cf_opts.set_block_based_table_factory(&opts);
 
     let zeros: [u8; 4096] = [0; 4096];
 
     let db =
-        DBWithThreadMode::<MultiThreaded>::open_default("_rust_rocksdb_multithreadtest").unwrap();
+        DBWithThreadMode::<MultiThreaded>::open(&cf_opts, "_rust_rocksdb_multithreadtest").unwrap();
     let db = Arc::new(db);
-
-    // Spawn all threads
-    for id in 0..DISK_PAGES {
-        let key: [u8; 8] = id.to_le_bytes();
-        let _ = db.put(key, zeros);
-        println!("id {}", id);
-    }
-
-    println!("Added all the pages");
 
     // Spawn all threads
     thread::scope(|s| {
@@ -59,12 +69,15 @@ fn bench() {
             s.spawn(move || {
                 let mut rng = rand::thread_rng();
                 for iteration in 0..ITERATIONS {
+                    // let mut zipf = zipf::ZipfDistribution::new(DISK_PAGES, 1.01).unwrap();
+                    // let id = zipf.sample(&mut rng) as u64;
                     let id = rng.gen_range(0..DISK_PAGES) as u64;
                     let key: [u8; 8] = id.to_le_bytes();
 
                     if coin.sample(&mut rng) {
                         local_db.put(key, zeros);
                     } else {
+                        READ_COUNTER.fetch_add(1, Ordering::SeqCst);
                         let slice = local_db.get(key);
                         std::hint::black_box(slice);
                     }
@@ -78,7 +91,13 @@ fn bench() {
             let duration = std::time::Duration::from_secs(1);
             let mut prev = 0;
             while COUNTER.load(Ordering::SeqCst) < THREADS * TASKS * ITERATIONS {
-                println!("Counter is at: {:?}", COUNTER.load(Ordering::SeqCst) - prev);
+                // println!(
+                //     "{:?} {:?} {:?}",
+                //     COUNTER.load(Ordering::SeqCst) - prev,
+                //     COUNTER.load(Ordering::SeqCst),
+                //     READ_COUNTER.load(Ordering::SeqCst)
+                // );
+                println!("{:?}", COUNTER.load(Ordering::SeqCst) - prev,);
                 prev = COUNTER.load(Ordering::SeqCst);
                 std::thread::sleep(duration);
             }
